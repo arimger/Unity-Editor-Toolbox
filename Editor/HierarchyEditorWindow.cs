5bcd0e574233c3fe0161889b500361c3bfa9029d
Milosz Matkowski <m.matkowski96@gmail.com>
2019-08-24 22:03:01 +0200

    Refactored HierarchyEditor. Fixed dragging handle OY position

--- Editor/HierarchyEditorWindow.cs
+++ Editor/HierarchyEditorWindow.cs
@@ -4,27 +4,65 @@ using UnityEditor;
 namespace Toolbox.Editor
 {
     [InitializeOnLoad]
-    public class HierarchyEditorWindow
+    public static class HierarchyEditorWindow
     {
         static HierarchyEditorWindow()
         {
-            EditorApplication.hierarchyWindowItemOnGUI += HierarchyItemCB;
+            EditorApplication.hierarchyWindowItemOnGUI += OnItemCallback;
         }
 
+        
+        /// <summary>
+        /// Delegate used in label element drawing.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        private delegate Rect DrawElementCallback(GameObject gameObject, Rect rect);
 
-        private static void HierarchyItemCB(int instanceID, Rect rect)
+        /// <summary>
+        /// Collection of all wanted hierarchy elements drawers.
+        /// </summary>
+        private static readonly DrawElementCallback[] DrawElementCallbacks = new DrawElementCallback[]
+        {       
+            DrawTag
+        };
+
+
+        private static void OnItemCallback(int instanceID, Rect rect)
         {
             var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
             if (gameObject)
             {
-                //TODO: draw in loop
-                rect = new Rect(rect.xMax - Style.maxWidth, rect.y, Style.maxWidth, rect.height);
-                rect = DrawIcon(gameObject, rect);
-                rect = new Rect(rect.xMin - Style.maxWidth, rect.y, Style.maxWidth, rect.height);
-                rect = DrawTag(gameObject, rect);
-                //rect = new Rect(rect.xMin - Style.maxWidth, rect.y, Style.maxWidth, rect.height);
-                //rect = DrawTag(gameObject, rect);
+                DrawItemLabel(gameObject, rect);
             }
+        }
+
+        /// <summary>
+        /// Draws label as whole. Creates separation line, standard icon and additional elements stored in collection.
+        /// Iterates over all neeeded draw callbacks and creates elements in horizontal group.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="rect"></param>
+        private static void DrawItemLabel(GameObject gameObject, Rect rect)
+        {
+            var contRect = rect;
+            contRect = new Rect(contRect.xMax - Style.maxWidth, rect.y, Style.maxWidth, contRect.height);
+            contRect = DrawIcon(gameObject, contRect);
+            foreach (var drawCallback in DrawElementCallbacks)
+            {
+                contRect =  new Rect(contRect.xMin - Style.maxWidth, rect.y, Style.maxWidth, contRect.height);
+                contRect = drawCallback(gameObject, contRect);
+            }
+
+            DrawLine(rect);
+        }
+
+        private static Rect DrawEmpty(GameObject gameObject, Rect rect)
+        {
+            EditorGUI.DrawRect(new Rect(rect.xMin, rect.y + Style.lineOffset / 2, Style.lineWidth, rect.height - Style.lineOffset), Color.grey);
+            EditorGUI.DrawRect(new Rect(rect.xMax, rect.y + Style.lineOffset / 2, Style.lineWidth, rect.height - Style.lineOffset), Color.grey);
+            return rect;
         }
 
         private static Rect DrawIcon(GameObject gameObject, Rect rect)
@@ -41,12 +79,15 @@ namespace Toolbox.Editor
             {
                 GUI.Label(contentRect, contentIcon);
             }
-
+           
             return contentRect;
         }
 
         private static Rect DrawTag(GameObject gameObject, Rect rect)
         {
+            EditorGUI.DrawRect(new Rect(rect.xMin, rect.y + Style.lineOffset / 2, Style.lineWidth, rect.height - Style.lineOffset), Style.lineColor);
+            EditorGUI.DrawRect(new Rect(rect.xMax, rect.y + Style.lineOffset / 2, Style.lineWidth, rect.height - Style.lineOffset), Style.lineColor);
+
             var content = new GUIContent(gameObject.tag);
 
             if (content.text != ToolboxEditorUtility.defaultUnityTag)
@@ -57,14 +98,27 @@ namespace Toolbox.Editor
             return rect;
         }
 
+        private static void DrawLine(Rect rect)
+        {
+            EditorGUI.DrawRect(new Rect(rect.x, rect.y + rect.height, rect.width, Style.lineWidth), Style.lineColor);
+        }
 
+
+        /// <summary>
+        /// Static representation of custom hierarchy style.
+        /// </summary>
         internal static class Style
         {
-            internal static float maxWidth = 60.0f;
+            internal static float maxWidth = 55.0f;
+            internal static float lineWidth = 1.0f;
+            internal static float lineOffset = 2.0f;
             internal static float iconWidth = 18.0f;
             internal static float iconHeight = 18.0f;
 
             internal static GUIStyle labelStyle;
+
+            internal static Color lineColor = Color.grey;
+
 
             static Style()
             {
