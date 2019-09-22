d01423613c50d96118c7df713d9170505da7e05d
Milosz Matkowski <milosz.matkowski96@gmail.com>
2019-09-22 19:34:21 +0200

    Fixed ToolboxEditor for built-in component. Added PresetAttribute&Drawer

---
+++ Scripts/Attributes/PresetAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+public class PresetAttribute : PropertyAttribute
+{
+    public PresetAttribute(string presetPropertyName)
+    {
+        PresetPropertyName = presetPropertyName;
+    }
+
+    public string PresetPropertyName { get; private set; }
+}
