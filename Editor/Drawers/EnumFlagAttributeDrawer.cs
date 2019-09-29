ca53ecb1050f52e9c9e53cb8711faed0bd733ccb
Milosz Matkowski <milosz.matkowski96@gmail.com>
2019-09-29 21:32:19 +0200

    Additional styling for EnumFlagAttribute drawer

--- Editor/Drawers/EnumFlagAttributeDrawer.cs
+++ Editor/Drawers/EnumFlagAttributeDrawer.cs
@@ -8,13 +8,55 @@ namespace Toolbox.Editor
     [CustomPropertyDrawer(typeof(EnumFlagAttribute))]
     public class EnumFlagAttributeDrawer : PropertyDrawer
     {
+        private const float mininumButtonWidth = 60.0f;
+
+
+        private float additionalHeight;
+
+
+        private void HandlePopupStyle(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var enumValue = fieldInfo.GetValue(property.serializedObject.targetObject) as Enum;
+            EditorGUI.BeginProperty(position, label, property);
+            property.intValue = Convert.ToInt32(EditorGUI.EnumFlagsField(position, label, enumValue));
+            EditorGUI.EndProperty();
+        }
+
+        private void HandleButtonStyle(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var enumValue = 0;
+            var maxWidth = EditorGUIUtility.labelWidth;
+            var maxHeight = EditorGUIUtility.singleLineHeight;
+            var buttonsCount = property.enumDisplayNames.Length;
+            var buttonsStates = new bool[buttonsCount];
+
+            EditorGUI.BeginProperty(position, label, property);
+            EditorGUI.BeginChangeCheck();
+
+            for (var i = 0; i < 0; i++)
+            {
+                for (var j = 0; j < 0; j++)
+                {
+                    
+                }
+            }
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.intValue = enumValue;
+            }
+        }
+
+
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return base.GetPropertyHeight(property, label);
+            return base.GetPropertyHeight(property, label) + additionalHeight;
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            additionalHeight = 0;
+
             if (property.propertyType != SerializedPropertyType.Enum)
             {
                 Debug.LogWarning(property.name + " property in " + property.serializedObject.targetObject +
@@ -23,10 +65,32 @@ namespace Toolbox.Editor
                 return;
             }
 
-            var enumValue = (Enum)fieldInfo.GetValue(property.serializedObject.targetObject);
-            EditorGUI.BeginProperty(position, label, property);
-            property.intValue = Convert.ToInt32(EditorGUI.EnumFlagsField(position, label, enumValue));
-            EditorGUI.EndProperty();
+            switch (Attribute.Style)
+            {
+                case EnumStyle.Popup:
+                    HandlePopupStyle(position, property, label);
+                    return;
+                case EnumStyle.Button:
+                    HandleButtonStyle(position, property, label);
+                    return;
+                default:
+                    HandlePopupStyle(position, property, label);
+                    return;
+            }
+        }
+
+
+        private EnumFlagAttribute Attribute => attribute as EnumFlagAttribute;
+
+
+        private static class Style
+        {
+            internal static readonly GUIStyle toggleStyle;
+
+            static Style()
+            {
+                toggleStyle = new GUIStyle(GUI.skin.button);
+            }
         }
     }
 }
