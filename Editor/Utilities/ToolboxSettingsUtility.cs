31a8441c3d94b69c95223fc9733e1a28ba9345c3
Milosz Matkowski <milosz.matkowski96@gmail.com>
2019-11-24 22:39:52 +0100

    Add custom icon picker in FolderDataDrawer

--- Editor/Utilities/ToolboxSettingsUtility.cs
+++ Editor/Utilities/ToolboxSettingsUtility.cs
@@ -12,9 +12,12 @@ namespace Toolbox.Editor
             const string settingsFileExt = ".asset";
             const string versionFileName = "Version.txt";
 
+            //find all possible settings files in whole project solution
             var guids = AssetDatabase.FindAssets("t:" + nameof(ToolboxEditorSettings));
+
             if (guids == null || guids.Length == 0) return;
 
+            //try to get proper path for first settings file
             var settingsFilePath = AssetDatabase.GUIDToAssetPath(guids[0]);
             Settings = AssetDatabase.LoadAssetAtPath<ToolboxEditorSettings>(settingsFilePath);
 
@@ -25,15 +28,18 @@ namespace Toolbox.Editor
                 return;
             }
 
+            //try to get version file path, it should be in same location as settings file
             var versionFilePath = settingsFilePath.Replace(Settings.name + settingsFileExt, versionFileName);
             Version = AssetDatabase.LoadAssetAtPath<TextAsset>(versionFilePath)?.text;
 
+            //initialize core functionalities
             ToolboxDrawerUtility.InitializeDrawers(Settings);
             ToolboxFolderUtility.InitializeProject(Settings);
         }
 
         internal static void ReimportSettings()
         {
+            //find and re-import this script file
             var guids = AssetDatabase.FindAssets(nameof(ToolboxSettingsUtility));
             if (guids == null || guids.Length == 0) return;
             var path = AssetDatabase.GUIDToAssetPath(guids[0]);
