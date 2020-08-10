274dc05072fbc759c6077e6ce3a1c6a6ae4fc1a7
Milosz Matkowski <milosz.matkowski96@gmail.com>
2020-08-10 21:16:56 +0200

    Add the possibility to hide/show particular components

--- Assets/Editor Toolbox/Editor/Utilities/ComponentUtility.cs
+++ Assets/Editor Toolbox/Editor/Utilities/ComponentUtility.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace Toolbox.Editor
@@ -46,13 +47,17 @@ namespace Toolbox.Editor
         private static void Paste()
         {
             var selectedGameobjects = Selection.gameObjects;
-
+         
             for (int i = 0; i < selectedGameobjects.Length; i++)
             {
+                var gameObject = selectedGameobjects[i];
+                Undo.RecordObjects(selectedGameobjects, "Copy components to selected GameObjects");
+
                 for (int j = 0; j < copiedComponents.Count; j++)
                 {
-                    UnityEditorInternal.ComponentUtility.CopyComponent(copiedComponents[j]);
-                    UnityEditorInternal.ComponentUtility.PasteComponentAsNew(selectedGameobjects[i].gameObject);
+                    var component = copiedComponents[j];
+                    UnityEditorInternal.ComponentUtility.CopyComponent(component);
+                    UnityEditorInternal.ComponentUtility.PasteComponentAsNew(gameObject);
                 }
             }
 
@@ -63,6 +68,47 @@ namespace Toolbox.Editor
         private static bool ValidatePaste()
         {
             return Selection.gameObjects.Length > 0 && copiedComponents.Count > 0;
+        }
+
+        [MenuItem("CONTEXT/Component/Hide Component", false, priority = 300)]
+        private static void Hide(MenuCommand menuCommand)
+        {
+            var component = menuCommand.context as Component;
+            Undo.RecordObject(component, "Hide " + component.GetType() + " in " + component.name);
+            component.hideFlags |= HideFlags.HideInInspector;
+            EditorUtility.SetDirty(component);
+        }
+        
+        [MenuItem("CONTEXT/Component/Hide All Components", false, priority = 301)]
+        private static void HideAll(MenuCommand menuCommand)
+        {
+            var gameObject = (menuCommand.context as Component).gameObject;
+            var components = (menuCommand.context as Component).GetComponents<Component>();
+
+            Undo.RecordObject(gameObject, "Hide all components in " + gameObject.name);
+
+            for (var i = 0; i < components.Length; i++)
+            {
+                components[i].hideFlags |= HideFlags.HideInInspector;
+            }
+
+            EditorUtility.SetDirty(gameObject);
+        }
+
+        [MenuItem("CONTEXT/Component/Show All Components", false, priority = 302)]
+        private static void ShowAll(MenuCommand menuCommand)
+        {
+            var gameObject = (menuCommand.context as Component).gameObject;
+            var components = (menuCommand.context as Component).GetComponents<Component>();
+
+            Undo.RecordObject(gameObject, "Show all components in " + gameObject.name);
+
+            for (var i = 0; i < components.Length; i++)
+            {
+                components[i].hideFlags &= ~HideFlags.HideInInspector;
+            }
+
+            EditorUtility.SetDirty(gameObject);
         }
 
 
