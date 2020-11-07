735b5641add2182fc7c47352975245a5b16a1b7e
Milosz Matkowski <milosz.matkowski96@gmail.com>
2020-11-07 22:45:25 +0100

    Allow comparison between the UnityEngine.Object reference and boolean value within condition attributes

--- Scripts/Attributes/ToolboxAttributes/ConditionAttributes/HideIfAttribute.cs
+++ Scripts/Attributes/ToolboxAttributes/ConditionAttributes/HideIfAttribute.cs
@@ -4,7 +4,7 @@ namespace UnityEngine
 {
     /// <summary>
     /// Hides property if provided condition is met. Conditional property has to be serialized.
-    /// Supported condition types: <see cref="bool"/>, <see cref="int"/>, <see cref="float"/>, <see cref="double"/>, <see cref="string"/> and any <see cref="Enum"/>. 
+    /// Supported condition types: <see cref="bool"/>, <see cref="int"/>, <see cref="float"/>, <see cref="double"/>, <see cref="string"/>, any <see cref="Enum"/>, and <see cref="Object"/> (but has to be compared to a <see cref="bool"/> value). 
     /// </summary>
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
     public class HideIfAttribute : ComparisonAttribute
