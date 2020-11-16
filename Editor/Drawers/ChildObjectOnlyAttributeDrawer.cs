ef90fda3232ce994fb9eb56ffd677d4661821cd2
Milosz Matkowski <milosz.matkowski96@gmail.com>
2020-11-16 22:20:44 +0100

    Add ChildObjectOnlyAttribute&Drawer

---
+++ Editor/Drawers/ChildObjectOnlyAttributeDrawer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Toolbox.Editor.Drawers
+{
+    [CustomPropertyDrawer(typeof(ChildObjectOnlyAttribute))]
+    public class ChildObjectOnlyAttributeDrawer : ToolboxNativePropertyDrawer
+    {
+        private Transform GetTransform(Object reference)
+        {
+            switch (reference)
+            {
+                case GameObject gameObject:
+                    return gameObject.transform;
+                case Component component:
+                    return component.transform;
+            }
+
+            return null;
+        }
+
+
+        protected override float GetPropertyHeightSafe(SerializedProperty property, GUIContent label)
+        {
+            return base.GetPropertyHeightSafe(property, label);
+        }
+
+        protected override void OnGUISafe(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(position, property, label, false);
+            if (EditorGUI.EndChangeCheck())
+            {
+                var target = property.serializedObject.targetObject as Component;
+                var reference = property.objectReferenceValue;
+                if (reference)
+                {
+                    var transfrom = GetTransform(reference);
+                    if (transfrom && transfrom != target.transform && transfrom.IsChildOf(target.transform))
+                    {
+                        return;
+                    }
+
+                    property.objectReferenceValue = null;
+                    ToolboxEditorLog.AttributeUsageWarning(attribute, property, "Assigned value has to be a child of the target transform.");
+                }
+            }
+        }
+
+
+        public override bool IsPropertyValid(SerializedProperty property)
+        {
+            return property.propertyType == SerializedPropertyType.ObjectReference;
+        }
+    }
+}
---
+++ Scripts/Attributes/ChildObjectOnlyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityEngine
+{    
+    /// <summary>
+    /// Accepts only child <see cref="GameObject"/>s and <see cref="Component"/>s.
+    /// Supported types: any <see cref="Object"/>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class ChildObjectOnlyAttribute : PropertyAttribute
+    { }
+}
