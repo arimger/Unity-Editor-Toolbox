b7739b772b4804e39a49c827ee7ec29ac6088ac1
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-04-07 23:14:55 +0200

    Update documentation

---
+++ Editor/Internal/DisabledScope.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace Toolbox.Editor.Internal
+{
+    internal class DisabledScope : IDisposable
+    {
+        private bool wasEnabled;
+
+        public DisabledScope(bool isEnabled)
+        {
+            Prepare(isEnabled);
+        }
+
+        public void Prepare(bool isEnabled)
+        {
+            wasEnabled = GUI.enabled;
+            GUI.enabled = isEnabled;
+        }
+
+        public void Dispose()
+        {
+            GUI.enabled = wasEnabled;
+        }
+    }
+}
