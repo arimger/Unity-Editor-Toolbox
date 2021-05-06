dff39e7b9e9323d5cf874c11eaebac18bec06ed0
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-05-06 19:30:32 +0200

    Add ShowWarningIfAttribute&Drawer

---
+++ Assets/Editor Toolbox/Editor/Drawers/ToolboxDrawers/ConditionDrawers/ShowWarningIfAttributeDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Toolbox.Editor.Drawers
+{
+    public class ShowWarningIfAttributeDrawer : ComparisonAttributeDrawer<ShowWarningIfAttribute>
+    {
+        protected override PropertyCondition OnGuiValidateSafe(SerializedProperty property, ShowWarningIfAttribute attribute)
+        {
+            var result = base.OnGuiValidateSafe(property, attribute);
+            if (result == PropertyCondition.Disabled)
+            {
+                EditorGUILayout.HelpBox(attribute.Message, MessageType.Warning);
+                result = attribute.DisableField 
+                    ? PropertyCondition.Disabled 
+                    : PropertyCondition.Valid;
+            }
+
+            return result;
+        }
+
+        protected override PropertyCondition OnComparisonResult(bool result)
+        {
+            return result ? PropertyCondition.Disabled : PropertyCondition.Valid;
+        }
+    }
+}
---
+++ Assets/Editor Toolbox/Scripts/Attributes/ToolboxAttributes/ConditionAttributes/ShowWarningIfAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// Shows warning message if provided condition is met. Conditional property has to be serialized.
+    /// 
+    /// <para>Supported condition types: <see cref="bool"/>, <see cref="int"/>, <see cref="float"/>, <see cref="double"/>, <see cref="string"/>, any <see cref="Enum"/>, and <see cref="Object"/> (but has to be compared to a <see cref="bool"/> value).</para>
+    /// <para>Supported types: all.</para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class ShowWarningIfAttribute : ComparisonAttribute
+    {
+        public ShowWarningIfAttribute(string propertyName, object valueToMatch, string message) : base(propertyName, valueToMatch)
+        {
+            Message = message;
+        }
+
+        public string Message { get; private set; }
+
+        public bool DisableField { get; set; } = true;
+    }
+}
