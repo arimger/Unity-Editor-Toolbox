494ba356040535b5ece3c6d6da5feff0a6d3599e
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-05-08 20:52:42 +0200

    Add OnValueChangedAttribute&Drawer

---
+++ Scripts/Attributes/RegularAttributes/OnValueChangedAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// TODO.
+    /// 
+    /// <para>Supported types: all.</para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class OnValueChangedAttribute : PropertyAttribute
+    {
+        public OnValueChangedAttribute(string callbackMethodName)
+        {
+            CallbackMethodName = callbackMethodName;
+        }
+
+        public string CallbackMethodName { get; private set; }
+    }
+}
