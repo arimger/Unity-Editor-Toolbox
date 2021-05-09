5bda9b0d2d5472e47cc1397c201731f0e945a225
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-05-09 23:36:07 +0200

    Add composition attributes

---
+++ Scripts/Attributes/ToolboxAttributes/CompositionAttributes/LabelWithLineAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityEngine
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    public class LabelWithLineAttribute : ToolboxCompositionAttribute
+    {
+        public LabelWithLineAttribute(string label)
+        {
+            Label = label;
+        }
+
+
+        public override ToolboxAttribute[] Process()
+        {
+            return new ToolboxAttribute[]
+            {
+                //NOTE: unfortunately we have to remove standard spacing between controls
+                new LabelAttribute(Label)
+                {
+#if UNITY_EDITOR
+                    SpaceAfter = -EditorGUIUtility.standardVerticalSpacing
+#endif
+                },
+                new LineAttribute(padding:0)
+            };
+        }
+
+
+        public string Label { get; private set; }
+    }
+}
---
+++ Scripts/Attributes/ToolboxAttributes/ToolboxCompositionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// Base class for all attributes responsible for the creation of dedicated composition of <see cref="ToolboxAttribute"/>s.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
+    public abstract class ToolboxCompositionAttribute : ToolboxAttribute
+    {
+        public abstract ToolboxAttribute[] Process();
+    }
+}
