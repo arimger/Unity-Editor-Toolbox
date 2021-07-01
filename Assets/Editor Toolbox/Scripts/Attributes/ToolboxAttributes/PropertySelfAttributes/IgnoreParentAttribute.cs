c3e12e2c20ed014e42b329cfd01efd0d7300bec9
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-07-01 09:46:26 +0200

    Add IgnoreParentAttribute&Drawer

---
+++ Assets/Editor Toolbox/Scripts/Attributes/ToolboxAttributes/PropertySelfAttributes/IgnoreParentAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityEngine
+{
+    /// <summary>
+    /// Ignores parent label and default foldout for children-based properties.
+    /// 
+    /// <para>Supported types: any.</para>
+    /// </summary>
+    public class IgnoreParentAttribute : ToolboxSelfPropertyAttribute
+    { }
+}
