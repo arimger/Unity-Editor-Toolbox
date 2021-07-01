4cb0fb3eb17f851c08f0cc6cddca96e03acf789f
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-07-01 09:50:44 +0200

    Add Show/HideDisabledIfAttribute&Drawer

---
+++ Assets/Editor Toolbox/Scripts/Attributes/ToolboxAttributes/ConditionAttributes/ShowDisabledIfAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// Shows and disables serialized field if the provided condition is met, otherwise the field is hidden.
+    /// Conditional property has to be serialized.
+    /// 
+    /// <para>Supported condition types: <see cref="bool"/>, <see cref="int"/>, <see cref="float"/>, <see cref="double"/>, <see cref="string"/>, any <see cref="Enum"/>, and <see cref="Object"/> (but has to be compared to a <see cref="bool"/> value).</para>
+    /// <para>Supported types: all.</para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class ShowDisabledIfAttribute : ComparisonAttribute
+    {
+        public ShowDisabledIfAttribute(string propertyName, object valueToMatch) : base(propertyName, valueToMatch)
+        { }
+    }
+}
