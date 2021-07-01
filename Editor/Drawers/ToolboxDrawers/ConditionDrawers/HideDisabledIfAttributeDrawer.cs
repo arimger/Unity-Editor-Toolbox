3d18ecad95a36e78809f548a3ddb43d84fa28a20
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-07-01 09:50:44 +0200

    Add Show/HideDisabledIfAttribute&Drawer

---
+++ Editor/Drawers/ToolboxDrawers/ConditionDrawers/HideDisabledIfAttributeDrawer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Toolbox.Editor.Drawers
+{
+    public class HideDisabledIfAttributeDrawer : ComparisonAttributeDrawer<HideDisabledIfAttribute>
+    {
+        protected override PropertyCondition OnComparisonResult(bool result)
+        {
+            return result ? PropertyCondition.NonValid : PropertyCondition.Disabled;
+        }
+    }
+}
---
+++ Scripts/Attributes/ToolboxAttributes/ConditionAttributes/HideDisabledIfAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// Hides serialized field if the provided condition is met, otherwise keeps it disabled. 
+    /// Conditional property has to be serialized.
+    /// 
+    /// <para>Supported condition types: <see cref="bool"/>, <see cref="int"/>, <see cref="float"/>, <see cref="double"/>, <see cref="string"/>, any <see cref="Enum"/>, and <see cref="Object"/> (but has to be compared to a <see cref="bool"/> value).</para>
+    /// <para>Supported types: all.</para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class HideDisabledIfAttribute : ComparisonAttribute
+    {
+        public HideDisabledIfAttribute(string propertyName, object valueToMatch) : base(propertyName, valueToMatch)
+        { }
+    }
+}
