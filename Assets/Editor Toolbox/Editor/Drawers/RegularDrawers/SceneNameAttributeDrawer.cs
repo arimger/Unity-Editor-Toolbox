d403e1c4106b3216e2483904b6e4c8b0ede02b43
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-07-09 19:23:50 +0200

    Add possibility to pick Scenes in the SceneNameAttributeDrawer

--- Assets/Editor Toolbox/Editor/Drawers/RegularDrawers/SceneNameAttributeDrawer.cs
+++ Assets/Editor Toolbox/Editor/Drawers/RegularDrawers/SceneNameAttributeDrawer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Toolbox.Editor.Drawers
 {
@@ -13,9 +14,9 @@ namespace Toolbox.Editor.Drawers
                 return false;
             }
 
-            for (var i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings; i++)
+            for (var i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
             {
-                var scenePath = UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i);
+                var scenePath = SceneUtility.GetScenePathByBuildIndex(i);
                 var lastSlash = scenePath.LastIndexOf("/");
                 var name = scenePath.Substring(lastSlash + 1, scenePath.LastIndexOf(".") - lastSlash - 1);
 
@@ -40,18 +41,40 @@ namespace Toolbox.Editor.Drawers
         {
             if (!SceneExists(property.stringValue))
             {
-                //set rect for the warning message field
                 var helpBoxRect = new Rect(position.x,
                                            position.y,
                                            position.width, Style.boxHeight);
                 EditorGUI.HelpBox(helpBoxRect, "Scene does not exist. " +
                                                "Check available Scenes in the Build options.", MessageType.Warning);
-
-                //adjust rect for standard property field
                 position.yMin += Style.boxHeight + Style.spacing * 2;
             }
 
-            EditorGUI.PropertyField(position, property, label, property.isExpanded);
+            position.height = Style.rowHeight;
+            position.xMax -= Style.pickerWidth + Style.spacing;
+            EditorGUI.PropertyField(position, property, label);
+            position.xMin += position.width;
+            position.xMax += Style.pickerWidth + Style.spacing;
+
+            var controlId = GUIUtility.GetControlID(FocusType.Keyboard);
+            if (GUI.Button(position, Style.pickerButtonContent, EditorStyles.miniButton))
+            {
+                EditorGUIUtility.ShowObjectPicker<SceneAsset>(null, false, null, controlId);
+            }
+
+            if (Event.current.commandName == "ObjectSelectorUpdated")
+            {
+                if (controlId == EditorGUIUtility.GetObjectPickerControlID())
+                {
+                    var target = EditorGUIUtility.GetObjectPickerObject();
+                    if (target)
+                    {
+                        property.serializedObject.Update();
+                        property.stringValue = target.name;
+                        property.serializedObject.ApplyModifiedProperties();
+                        GUI.changed = true;
+                    }
+                }
+            }
         }
 
 
@@ -70,7 +93,14 @@ namespace Toolbox.Editor.Drawers
             internal static readonly float boxHeight = EditorGUIUtility.singleLineHeight * 2.5f;
 #endif
             internal static readonly float spacing = EditorGUIUtility.standardVerticalSpacing;
-            internal static readonly float padding = 5.0f;
+            internal static readonly float pickerWidth = 30.0f;
+
+            internal static readonly GUIContent pickerButtonContent;
+
+            static Style()
+            {
+                pickerButtonContent = new GUIContent(EditorGUIUtility.FindTexture("UnityLogoLarge"), "Pick SceneAsset");
+            }
         }
     }
 }
