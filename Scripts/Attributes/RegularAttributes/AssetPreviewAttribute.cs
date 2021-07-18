5bed6894d0c3b5b075a35d68be5ab5c4df9362c8
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-07-18 20:31:26 +0200

    Minor refactor changes

--- Scripts/Attributes/RegularAttributes/AssetPreviewAttribute.cs
+++ Scripts/Attributes/RegularAttributes/AssetPreviewAttribute.cs
@@ -12,14 +12,14 @@ namespace UnityEngine
     {
         public AssetPreviewAttribute(float width = 64, float height = 64, bool useLabel = true)
         {
-            Height = height;
             Width = width;
+            Height = height;
             UseLabel = useLabel;
         }
 
-        public float Height { get; private set; }
-
         public float Width { get; private set; }
+
+        public float Height { get; private set; }
 
         public bool UseLabel { get; private set; }
     }
--- Scripts/Attributes/RegularAttributes/TypeConstraintAttribute.cs
+++ Scripts/Attributes/RegularAttributes/TypeConstraintAttribute.cs
@@ -7,31 +7,6 @@ using Toolbox;
 namespace UnityEngine
 {
     /// <summary>
-    /// Indicates how selectable classes should be collated in drop-down menu.
-    /// </summary>
-    public enum ClassGrouping
-    {
-        /// <summary>
-        /// No grouping, just show type names in a list; for instance, "Some.Nested.Namespace.SpecialClass".
-        /// </summary>
-        None,
-        /// <summary>
-        /// Group classes by namespace and show foldout menus for nested namespaces; for
-        /// instance, "Some > Nested > Namespace > SpecialClass".
-        /// </summary>
-        ByNamespace,
-        /// <summary>
-        /// Group classes by namespace; for instance, "Some.Nested.Namespace > SpecialClass".
-        /// </summary>
-        ByNamespaceFlat,
-        /// <summary>
-        /// Group classes in the same way as Unity does for its component menu. This
-        /// grouping method must only be used for <see cref="MonoBehaviour"/> types.
-        /// </summary>
-        ByAddComponentMenu,
-    }
-
-    /// <summary>
     /// Allows to pick proper type using popup control.
     /// 
     /// <para>Supported types: <see cref="SerializedType"/>.</para>
@@ -204,5 +179,30 @@ namespace UnityEngine
 
             return false;
         }
+    }
+
+    /// <summary>
+    /// Indicates how selectable classes should be collated in drop-down menu.
+    /// </summary>
+    public enum ClassGrouping
+    {
+        /// <summary>
+        /// No grouping, just show type names in a list; for instance, "Some.Nested.Namespace.SpecialClass".
+        /// </summary>
+        None,
+        /// <summary>
+        /// Group classes by namespace and show foldout menus for nested namespaces; for
+        /// instance, "Some > Nested > Namespace > SpecialClass".
+        /// </summary>
+        ByNamespace,
+        /// <summary>
+        /// Group classes by namespace; for instance, "Some.Nested.Namespace > SpecialClass".
+        /// </summary>
+        ByNamespaceFlat,
+        /// <summary>
+        /// Group classes in the same way as Unity does for its component menu. This
+        /// grouping method must only be used for <see cref="MonoBehaviour"/> types.
+        /// </summary>
+        ByAddComponentMenu,
     }
 }
