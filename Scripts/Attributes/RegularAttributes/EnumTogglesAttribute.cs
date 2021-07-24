a9428e23cd52008c53c907a0dbde318ddfbbc5ec
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-07-25 00:56:44 +0200

    Add EnumTogglesAttribute&Drawer

---
+++ Scripts/Attributes/RegularAttributes/EnumTogglesAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// Creates a group of toggles instead of the built-in popup menu.
+    /// 
+    /// <para>Supported types: any <see cref="Enum"/>.</para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class EnumTogglesAttribute : PropertyAttribute
+    {
+        /// <summary>
+        /// Minimum width of toggle created within the drawer.
+        /// </summary>
+        public float ToggleWidth { get; set; } = 85.0f;
+        /// <summary>
+        /// Height of a single toggle.
+        /// </summary>
+        public float ToggleHeight { get; set; } = 16.0f;
+        /// <summary>
+        /// Spacing between toggle buttons.
+        /// </summary>
+        public float ToggleSpacing { get; set; } = 2.0f;
+    }
+}
