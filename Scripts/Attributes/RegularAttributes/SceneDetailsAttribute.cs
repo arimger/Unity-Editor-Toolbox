f0b01b8285904b1e247e078bcb8317f6a789488b
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-07-26 19:46:37 +0200

    Add possibility to show Scene details in the SerializedScene drawer

---
+++ Scripts/Attributes/RegularAttributes/SceneDetailsAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// Indicates if <see cref="SerializedScene"/> drawer should show additional metadata about the picked Scene.
+    /// 
+    /// <para>Supported types: <see cref="SerializedScene"/>.</para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class SceneDetailsAttribute : PropertyAttribute
+    { }
+}
