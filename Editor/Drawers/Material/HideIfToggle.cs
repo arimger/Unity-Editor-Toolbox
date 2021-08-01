051a57d2d39dd90071eb9419f9e034a0852d3abc
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-08-01 22:54:59 +0200

    Base for shader-related drawers

---
+++ Editor/Drawers/Material/HideIfToggle.cs
@@ -0,0 +1,14 @@
+﻿namespace Toolbox.Editor.Drawers
+{
+    public class HideIfToggle : ConditionalDrawer
+    {
+        public HideIfToggle(string togglePropertyName) : base(togglePropertyName)
+        { }
+
+
+        protected override bool IsVisible(bool? value)
+        {
+            return value.HasValue ? !value.Value : false;
+        }
+    }
+}
---
+++ Editor/Drawers/Material/ShowIfToggle.cs
@@ -0,0 +1,14 @@
+﻿namespace Toolbox.Editor.Drawers
+{
+    public class ShowIfToggle : ConditionalDrawer
+    {
+        public ShowIfToggle(string togglePropertyName) : base(togglePropertyName)
+        { }
+
+
+        protected override bool IsVisible(bool? value)
+        {
+            return value.HasValue ? value.Value : false;
+        }
+    }
+}
