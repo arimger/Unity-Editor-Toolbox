a520b32e981c1c00e4a4a1dca455e474d2e871f4
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-08-04 22:51:03 +0200

    Add new material-related drawers: Indent, Title, Help

---
+++ Editor/Drawers/Material/ShowIfToggleDrawer.cs
@@ -0,0 +1,14 @@
+﻿namespace Toolbox.Editor.Drawers
+{
+    public class ShowIfToggleDrawer : ConditionalDrawer
+    {
+        public ShowIfToggleDrawer(string togglePropertyName) : base(togglePropertyName)
+        { }
+
+
+        protected override bool IsVisible(bool? value)
+        {
+            return value ?? false;
+        }
+    }
+}
