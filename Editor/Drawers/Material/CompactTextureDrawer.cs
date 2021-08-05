826dba2ff7fcb5d005d91eb8f0d9c7bb01012136
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-08-05 23:22:35 +0200

    Add new material drawers: CompactTexture, MinMaxSlider, Vector2, and Vector3

---
+++ Editor/Drawers/Material/CompactTextureDrawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Toolbox.Editor.Drawers
+{
+    public class CompactTextureDrawer : BaseMaterialPropertyDrawer
+    {
+        protected override float GetPropertyHeightSafe(MaterialProperty prop, string label, MaterialEditor editor)
+        {
+            return 0;
+        }
+
+        protected override void OnGUISafe(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
+        {
+            editor.TexturePropertySingleLine(new GUIContent(label), prop);
+        }
+
+        protected override bool IsPropertyValid(MaterialProperty prop)
+        {
+            return prop.type == MaterialProperty.PropType.Texture;
+        }
+    }
+}
---
+++ Editor/Drawers/Material/MinMaxSliderDrawer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Toolbox.Editor.Drawers
+{
+    using Toolbox.Editor.Internal;
+
+    public class MinMaxSliderDrawer : BaseMaterialPropertyDrawer
+    {
+        private readonly float minValue;
+        private readonly float maxValue;
+
+
+        public MinMaxSliderDrawer(float minValue, float maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+
+        protected override float GetPropertyHeightSafe(MaterialProperty prop, string label, MaterialEditor editor)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        protected override void OnGUISafe(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
+        {
+            using (new FixedFieldsScope())
+            {
+                EditorGUIUtility.labelWidth = 0;
+
+                var vectorValue = prop.vectorValue;
+                var xValue = vectorValue.x;
+                var yValue = vectorValue.y;
+
+                EditorGUI.BeginChangeCheck();
+                ToolboxEditorGui.DrawMinMaxSlider(position, label, ref xValue, ref yValue, minValue, maxValue);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    vectorValue.x = xValue;
+                    vectorValue.y = yValue;
+                    prop.vectorValue = vectorValue;
+                }
+            }
+        }
+
+        protected override bool IsPropertyValid(MaterialProperty prop)
+        {
+            return prop.type == MaterialProperty.PropType.Vector;
+        }
+    }
+}
