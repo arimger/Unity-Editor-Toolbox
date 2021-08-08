a5569719857a88cad14f682aa26cd124e97a5c6e
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-08-08 23:02:34 +0200

    Rect-based CompactTextureDrawer vesion

--- Editor/Drawers/Material/CompactTextureDrawer.cs
+++ Editor/Drawers/Material/CompactTextureDrawer.cs
@@ -5,14 +5,26 @@ namespace Toolbox.Editor.Drawers
 {
     public class CompactTextureDrawer : BaseMaterialPropertyDrawer
     {
+        private readonly string tooltip;
+
+
+        public CompactTextureDrawer() : this(string.Empty)
+        { }
+
+        public CompactTextureDrawer(string tooltip)
+        {
+            this.tooltip = tooltip;
+        }
+
+
         protected override float GetPropertyHeightSafe(MaterialProperty prop, string label, MaterialEditor editor)
         {
-            return 0;
+            return EditorGUIUtility.singleLineHeight;
         }
 
         protected override void OnGUISafe(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
         {
-            editor.TexturePropertySingleLine(new GUIContent(label), prop);
+            editor.TexturePropertyMiniThumbnail(position, prop, label, tooltip);
         }
 
         protected override bool IsPropertyValid(MaterialProperty prop)
