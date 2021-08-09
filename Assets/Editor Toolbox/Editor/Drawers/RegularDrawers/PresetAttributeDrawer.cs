5c79a735d59b39b57ab8dfa1f66b72a25598dfa0
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-08-09 20:23:36 +0200

    Minor refactor changes in the PresetAttributeDrawer

--- Assets/Editor Toolbox/Editor/Drawers/RegularDrawers/PresetAttributeDrawer.cs
+++ Assets/Editor Toolbox/Editor/Drawers/RegularDrawers/PresetAttributeDrawer.cs
@@ -19,82 +19,80 @@ namespace Toolbox.Editor.Drawers
         protected override void OnGUISafe(Rect position, SerializedProperty property, GUIContent label)
         {
             var sourceName = Attribute.SourceName;
-            var targetObject = property.GetDeclaringObject();
-            if (!ValueExtractionHelper.TryGetValue(sourceName, targetObject, out var presetSource))
+            var declaringObject = property.GetDeclaringObject();
+            //extract (if available) the real preset value
+            if (!ValueExtractionHelper.TryGetValue(sourceName, declaringObject, out var sourceValue))
             {
                 ToolboxEditorLog.AttributeUsageWarning(attribute, property,
-                    string.Format("Cannot find relative preset source ({0}).", sourceName));
+                    string.Format("Cannot find relative Preset ({0}).", sourceName));
                 EditorGUI.PropertyField(position, property, label);
                 return;
             }
 
-            if (presetSource is IList list)
-            {
-                var presetSourceType = presetSource.GetType();
-                var propertyType = property.GetProperType(fieldInfo, targetObject);
-                //check if types match between property and provided preset
-                if (propertyType == (presetSourceType.IsGenericType
-                                   ? presetSourceType.GetGenericArguments()[0]
-                                   : presetSourceType.GetElementType()))
-                {
-                    var objects = new object[list.Count];
-                    var options = new string[list.Count];
-
-                    for (var i = 0; i < list.Count; i++)
-                    {
-                        objects[i] = list[i];
-                        options[i] = list[i]?.ToString();
-                    }
-
-                    var index = Array.IndexOf(objects, property.GetProperValue(fieldInfo, targetObject));
-
-                    //begin the true property
-                    label = EditorGUI.BeginProperty(position, label, property);
-                    //draw the prefix label
-                    position = EditorGUI.PrefixLabel(position, label);
-
-                    EditorGUI.BeginChangeCheck();
-                    //get selected preset value
-                    index = EditorGUI.Popup(position, index, options);
-                    //validate index before set
-                    index = Mathf.Clamp(index, 0, list.Count - 1);
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        //udpate property value using previously cached FieldInfo and picked value
-                        //there is no cleaner way to do it, since we don't really know what kind of 
-                        //serialized property we are updating
-
-                        property.serializedObject.Update();
-                        property.SetProperValue(fieldInfo, objects[index]);
-                        property.serializedObject.ApplyModifiedProperties();
-
-                        //handle situation when updating multiple different targets
-                        property.serializedObject.SetIsDifferentCacheDirty();
-                    }
-
-                    EditorGUI.EndProperty();
-                }
-                else
-                {
-                    ToolboxEditorLog.AttributeUsageWarning(attribute, property,
-                        "Type mismatch between serialized property and provided preset field.");
-                    EditorGUI.PropertyField(position, property, label);
-                    return;
-                }
-            }
-            else
+            if (!(sourceValue is IList presetList))
             {
                 ToolboxEditorLog.AttributeUsageWarning(attribute, property,
-                    string.Format("Preset source ({0}) has to be a one-dimensional collection (array or list).", sourceName));
+                    string.Format("Preset ({0}) has to be a one-dimensional collection (array or list).", sourceName));
                 EditorGUI.PropertyField(position, property, label);
                 return;
             }
+
+            var sourceType = sourceValue.GetType();
+            var targetType = property.GetProperType(fieldInfo, declaringObject);
+            //check if types match between property and provided preset
+            if (targetType != (sourceType.IsGenericType
+                             ? sourceType.GetGenericArguments()[0]
+                             : sourceType.GetElementType()))
+            {
+                ToolboxEditorLog.AttributeUsageWarning(attribute, property,
+                    "Type mismatch between serialized property and given Preset.");
+                EditorGUI.PropertyField(position, property, label);
+                return;
+            }
+
+            var itemsCount = presetList.Count;
+            var objects = new object[itemsCount];
+            var options = new string[itemsCount];
+            for (var i = 0; i < itemsCount; i++)
+            {
+                objects[i] = presetList[i];
+                options[i] = presetList[i]?.ToString();
+            }
+
+            var value = property.GetProperValue(fieldInfo, declaringObject);
+            var index = Array.IndexOf(objects, value);
+
+            //begin the true property
+            label = EditorGUI.BeginProperty(position, label, property);
+            //draw the prefix label
+            position = EditorGUI.PrefixLabel(position, label);
+
+            EditorGUI.BeginChangeCheck();
+            //get selected preset value
+            index = EditorGUI.Popup(position, index, options);
+            index = Mathf.Clamp(index, 0, itemsCount - 1);
+            if (EditorGUI.EndChangeCheck())
+            {
+                //udpate property value using previously cached FieldInfo and picked value
+                //there is no cleaner way to do it, since we don't really know what kind of 
+                //serialized property we are updating
+
+                property.serializedObject.Update();
+                property.SetProperValue(fieldInfo, objects[index]);
+                property.serializedObject.ApplyModifiedProperties();
+                //handle situation when updating multiple different targets
+                property.serializedObject.SetIsDifferentCacheDirty();
+            }
+
+            EditorGUI.EndProperty();
         }
+
 
         public override bool IsPropertyValid(SerializedProperty property)
         {
+            var declaringObject = property.GetDeclaringObject();
             //NOTE: reflection won't work properly on nested structs because of boxing
-            return !property.GetDeclaringObject().GetType().IsValueType;
+            return !declaringObject.GetType().IsValueType;
         }
 
 
