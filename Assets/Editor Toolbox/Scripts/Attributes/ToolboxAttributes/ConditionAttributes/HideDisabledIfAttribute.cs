b4470e3dc1e14e30b8ba67638d9418a9da9cd663
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-08-13 17:57:26 +0200

    Update summaries

--- Assets/Editor Toolbox/Scripts/Attributes/ToolboxAttributes/ConditionAttributes/HideDisabledIfAttribute.cs
+++ Assets/Editor Toolbox/Scripts/Attributes/ToolboxAttributes/ConditionAttributes/HideDisabledIfAttribute.cs
@@ -3,16 +3,16 @@
 namespace UnityEngine
 {
     /// <summary>
-    /// Hides serialized field if the provided condition is met, otherwise keeps it disabled. 
-    /// Conditional property has to be serialized.
+    /// Hides serialized field if the provided condition is met, otherwise keeps it disabled.
     /// 
+    /// <para>Supported sources: fields, properties, and methods.</para>
     /// <para>Supported condition types: <see cref="bool"/>, <see cref="int"/>, <see cref="float"/>, <see cref="double"/>, <see cref="string"/>, any <see cref="Enum"/>, and <see cref="Object"/> (but has to be compared to a <see cref="bool"/> value).</para>
     /// <para>Supported types: all.</para>
     /// </summary>
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
     public class HideDisabledIfAttribute : ComparisonAttribute
     {
-        public HideDisabledIfAttribute(string propertyName, object valueToMatch) : base(propertyName, valueToMatch)
+        public HideDisabledIfAttribute(string sourceHandle, object valueToMatch) : base(sourceHandle, valueToMatch)
         { }
     }
 }
