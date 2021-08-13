975d96232b5e6a624de5bb324eefc6c9d5e0c089
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-08-13 18:03:39 +0200

    Add DynamicHelpAttribute&Drawer

---
+++ Editor/Drawers/Toolbox/DecoratorDrawers/DynamicHelpAttributeDrawer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Toolbox.Editor.Drawers
+{
+    public class DynamicHelpAttributeDrawer : ToolboxDecoratorDrawer<DynamicHelpAttribute>
+    {
+        protected override void OnGuiBeginSafe(DynamicHelpAttribute attribute)
+        {
+            //TODO:
+            //if (ValueExtractionHelper.TryGetValue(attribute.SourceHandle,))
+            EditorGUILayout.HelpBox(attribute.SourceHandle, (MessageType)attribute.Type);
+        }
+    }
+}
