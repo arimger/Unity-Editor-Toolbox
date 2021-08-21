96038f05c581447d3ceabfca107217163b05e73c
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-08-22 00:26:08 +0200

    Update reflection-related tests

--- Tests/Editor/ExtractionTest.cs
+++ Tests/Editor/ExtractionTest.cs
@@ -21,7 +21,7 @@ namespace Toolbox.Editor.Tests
         [TestCase("var2", "var1", 3)]
         [TestCase("var2.var3", "var1", "TestValue")]
         [TestCase("var2.var2.var2", "var1", true)]
-        public void TestExtractionPass(string propertyPath, string source, object expected)
+        public void TestFieldsExtractionPass(string propertyPath, string source, object expected)
         {
             var property = scriptableObject.FindProperty(propertyPath);
             var sibling = property.GetSibling(source);
@@ -30,6 +30,32 @@ namespace Toolbox.Editor.Tests
             ValueExtractionHelper.TryGetValue(source, property, out var actual, out var hasMixedValues);
             Assert.IsFalse(hasMixedValues);
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("var2", "BoolValue", true)]
+        [TestCase("var2.var3", "GetFloatValue", 1.6f)]
+        [TestCase("var2.var2.var2", "IntValue", 3)]
+        public void TestPropertiesExtractionPass(string propertyPath, string source, object expected)
+        {
+            var property = scriptableObject.FindProperty(propertyPath);
+            ValueExtractionHelper.TryGetValue(source, property, out var actual, out _);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("var2", "GetStringValue", "Text", true)]
+        [TestCase("var2", "GetDoubleValue", null, false)]
+        [TestCase("var2.var3", "GetFloatValue", 1.6f, true)]
+        [TestCase("var2.var2.var2", "GetIntValue", null, false)]
+        [TestCase("var2.var2.var2", "DoSomething", null, true)]
+        public void TestMethodsExtractionPass(string propertyPath, string source, object expected, bool success)
+        {
+            var property = scriptableObject.FindProperty(propertyPath);
+            var isFound = ValueExtractionHelper.TryGetValue(source, property, out var actual, out _);
+            Assert.AreEqual(success, isFound);
+            if (isFound)
+            {
+                Assert.AreEqual(expected, actual);
+            }
         }
     }
 }
--- Tests/Editor/TestObject2.cs
+++ Tests/Editor/TestObject2.cs
@@ -8,12 +8,32 @@ namespace Toolbox.Editor.Tests
         public int var1;
         public TestNestedObject1 var2;
 
+        public bool BoolValue
+        {
+            get => true;
+        }
+
+        public string GetStringValue()
+        {
+            return "Text";
+        }
+
+        public double GetDoubleValue(int arg)
+        {
+            return arg + 2.0005;
+        }
+
         [Serializable]
         public class TestNestedObject1
         {
             public string var1;
             public TestNestedObject2 var2;
             public float[] var3;
+
+            public float GetFloatValue()
+            {
+                return 1.6f;
+            }
         }
 
         [Serializable]
@@ -21,6 +41,14 @@ namespace Toolbox.Editor.Tests
         {
             public bool var1;
             public int var2;
+
+            public int IntValue
+            {
+                get => 3;
+            }
+
+            public void DoSomething()
+            { }
         }
     }
 }
