01ffbe54447919f158c3700604b90e54feeac8af
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2021-11-11 13:53:33 +0100

    Add DynamicRangeAttribute&Drawer

---
+++ Assets/Editor Toolbox/Scripts/Attributes/ToolboxAttributes/PropertySelfAttributes/DynamicRangeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class DynamicRangeAttribute : ToolboxSelfPropertyAttribute
+    {
+        public DynamicRangeAttribute(string minValueSource, string maxValueSource)
+        {
+            MinValueSource = minValueSource;
+            MaxValueSource = maxValueSource;
+        }
+
+        public string MinValueSource { get; private set; }
+
+        public string MaxValueSource { get; private set; }
+    }
+}
