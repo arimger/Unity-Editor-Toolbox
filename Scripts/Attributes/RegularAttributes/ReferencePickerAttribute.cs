080fcb4ae138d2c332f14247f203338e2d8cd243
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-03-02 21:50:15 +0100

    Add ReferencePickerAttribute

---
+++ Scripts/Attributes/RegularAttributes/ReferencePickerAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// Creates dedicated drawer for fields marked with <see cref="SerializeReference"/>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    [Conditional("UNITY_EDITOR")]
+    public class ReferencePickerAttribute : PropertyAttribute
+    { }
+}
