7273a9d5a044690e3ed22eada193e9b70ffd6a57
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-04-03 00:13:49 +0200

    WIP TypeUtilities class

---
+++ Assets/Editor Toolbox/Editor/Utilities/TypeUtilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEditor;
+
+namespace Toolbox.Editor
+{
+    public static class TypeUtilities
+    {
+        private static readonly Dictionary<int, List<Type>> cachedTypes = new Dictionary<int, List<Type>>();
+
+
+        public static List<Type> GetTypes(Type parentType)
+        {
+            var types = TypeCache.GetTypesDerivedFrom(parentType).ToList();
+            for (var i = types.Count - 1; i >= 0; i--)
+            {
+                var type = types[i];
+                //if (IsConstraintSatisfied(type))
+                {
+                    continue;
+                }
+
+                //types.RemoveAt(i);
+            }
+
+            return types;
+        }
+    }
+}
