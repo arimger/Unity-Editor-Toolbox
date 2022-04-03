e279e2166f6225f6fb53dc146df3064769394387
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-04-03 22:48:01 +0200

    WIP new type caching&management system

---
+++ Editor/Internal/TypeCachedInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toolbox.Editor.Internal
+{
+    public class TypeCachedInfo
+    {
+        private readonly string[] options;
+        private readonly List<Type> types;
+
+
+        public TypeCachedInfo(List<Type> types)
+        {
+            this.types = types;
+            var count = types.Count;
+            options = new string[count];
+            for (var i = 0; i < count; i++)
+            {
+                var type = types[i];
+                var name = type.FullName;
+                options[i] = name;
+            }
+        }
+
+
+        public int IndexOf(Type type)
+        {
+            return types.IndexOf(type);
+        }
+    }
+}
---
+++ Editor/Internal/TypeField.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Toolbox.Editor.Internal
+{
+    public class TypeField
+    {
+        private bool allowAbstract;
+        private bool allowObsolete;
+        private bool addSearchField;
+
+
+        public void OnGui(Rect position, Type parentType)
+        {
+            //TODO:
+            //ToolboxEditorGui.DrawSearchablePopup(position, buttonLabel, index, options, (i) =>
+            //{
+            //    try
+            //    {
+            //        //TODO: handle multiple objects
+            //        property.serializedObject.Update();
+            //        var type = i >= 0 ? filteredTypes[i] : null;
+            //        var obj = type != null ? Activator.CreateInstance(type) : null;
+            //        property.managedReferenceValue = obj;
+            //        property.serializedObject.ApplyModifiedProperties();
+            //    }
+            //    catch (Exception e) when (e is ArgumentNullException || e is NullReferenceException)
+            //    {
+            //        ToolboxEditorLog.LogWarning("Invalid attempt to update disposed property.");
+            //    }
+            //});
+        }
+
+
+        public IReadOnlyList<Type> CachedTypes { get; private set; }
+    }
+}
