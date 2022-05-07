9ee4a056f8093b33b27632f02f137cfe3bb4794b
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-05-07 23:51:00 +0200

    Minor refactor changes

--- Assets/Editor Toolbox/Editor/Drawers/Toolbox/ToolboxDecoratorDrawer.cs
+++ Assets/Editor Toolbox/Editor/Drawers/Toolbox/ToolboxDecoratorDrawer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Toolbox.Editor.Drawers
 {
     public abstract class ToolboxDecoratorDrawer<T> : ToolboxDecoratorDrawerBase where T : ToolboxDecoratorAttribute
-    {        
+    {
         protected virtual void OnGuiBeginSafe(T attribute)
         { }
 
@@ -17,12 +17,12 @@ namespace Toolbox.Editor.Drawers
         { }
 
 
-        public override sealed void OnGuiBegin(ToolboxAttribute attribute)
+        public sealed override void OnGuiBegin(ToolboxAttribute attribute)
         {
             OnGuiBegin(attribute as T);
         }
 
-        public override sealed void OnGuiClose(ToolboxAttribute attribute)
+        public sealed override void OnGuiClose(ToolboxAttribute attribute)
         {
             OnGuiClose(attribute as T);
         }
