1a0f1c390bdde0436d7f5aa88fe8eb252ac99a48
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-05-17 01:04:22 +0200

    Minor refactor changes

--- Assets/Editor Toolbox/Runtime/Attributes/Toolbox/DecoratorAttributes/EndGroupAttribute.cs
+++ Assets/Editor Toolbox/Runtime/Attributes/Toolbox/DecoratorAttributes/EndGroupAttribute.cs
@@ -3,6 +3,9 @@ using System.Diagnostics;
 
 namespace UnityEngine
 {
+    /// <summary>
+    /// Use this attribute to close previously created <see cref="BeginGroupAttribute"/>.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
     [Conditional("UNITY_EDITOR")]
     public class EndGroupAttribute : ToolboxDecoratorAttribute
