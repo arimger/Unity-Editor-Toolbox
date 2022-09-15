101a9c5787334e5d38e3f705a10058b24b97c153
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-09-15 23:08:22 +0200

    Utility methods for the EditorPrefs API

--- Assets/Editor Toolbox/Editor/ToolboxPrefs.cs
+++ Assets/Editor Toolbox/Editor/ToolboxPrefs.cs
@@ -1,23 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 
 using UnityEditor;
 
 namespace Toolbox.Editor
 {
+    /// <summary>
+    /// Additional overlay for <see cref="EditorPrefs"/>.
+    /// This utility class is Work in Progress.
+    /// </summary>
     internal static class ToolboxPrefs
     {
-        private const string separator = ".";
-        private const string keyBase = "Toolbox";
-
-        private static readonly HashSet<string> usedKeys = new HashSet<string>();
-
-
-        private static string GetKey(object causer, string propertyName)
-        {
-            return GetKey(causer.GetType(), propertyName);
-        }
-
         private static string GetKey(Type causer, string propertyName)
         {
             return GetKey(causer.Name, propertyName);
@@ -25,7 +17,7 @@ namespace Toolbox.Editor
 
         private static string GetKey(string causer, string propertyName)
         {
-            return string.Format("{0}{3}{1}{3}{2}", keyBase, causer, propertyName, separator);
+            return string.Format("{0}{3}{1}{3}{2}", "Toolbox", causer, propertyName, ".");
         }
 
 
@@ -34,20 +26,48 @@ namespace Toolbox.Editor
             EditorPrefs.DeleteAll();
         }
 
-        public static void DeleteCached()
+        public static bool GetBool(object causer, string propertyName, bool defaultValue = false)
         {
-            //TODO:
+            return GetBool(causer.GetType(), propertyName, defaultValue);
         }
 
-        public static bool GetBool(string causer, string propertyName, bool defaultValue)
+        public static bool GetBool(Type causer, string propertyName, bool defaultValue = false)
         {
             var key = GetKey(causer, propertyName);
             return EditorPrefs.GetBool(key, defaultValue);
         }
 
-        public static T GetValue<T>(string causer, string propertyName, T defaultValue)
+        public static void SetBool(object causer, string propertyName, bool value)
         {
-            return default;
+            SetBool(causer.GetType(), propertyName, value);
+        }
+
+        public static void SetBool(Type causer, string propertyName, bool value)
+        {
+            var key = GetKey(causer, propertyName);
+            EditorPrefs.SetBool(key, value);
+        }
+
+        public static int GetInt(object causer, string propertyName, int defaultValue = 0)
+        {
+            return GetInt(causer.GetType(), propertyName, defaultValue);
+        }
+
+        public static int GetInt(Type causer, string propertyName, int defaultValue = 0)
+        {
+            var key = GetKey(causer, propertyName);
+            return EditorPrefs.GetInt(key, defaultValue);
+        }
+
+        public static void SetInt(object causer, string propertyName, int value)
+        {
+            SetInt(causer.GetType(), propertyName, value);
+        }
+
+        public static void SetInt(Type causer, string propertyName, int value)
+        {
+            var key = GetKey(causer, propertyName);
+            EditorPrefs.SetInt(key, value);
         }
     }
 }
