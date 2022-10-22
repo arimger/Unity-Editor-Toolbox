fa9827d5b05fe628fc3c75ae2646d8ae323e417a
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-10-23 01:13:00 +0200

    Basic implementation of custom Context Menus for SerializedProperties

---
+++ Editor/ContextMenuController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Toolbox.Editor
+{
+    [InitializeOnLoad]
+    internal static class ContextMenuController
+    {
+        static ContextMenuController()
+        {
+            EditorApplication.contextualPropertyMenu -= OnContextMenuOpening;
+            EditorApplication.contextualPropertyMenu += OnContextMenuOpening;
+        }
+
+        private static void OnContextMenuOpening(GenericMenu menu, SerializedProperty property)
+        {
+            HandleSerializeReference(menu, property);
+        }
+
+        //TODO: dedicated class to handle it
+        private static void HandleSerializeReference(GenericMenu menu, SerializedProperty property)
+        {
+            if (property.propertyType != SerializedPropertyType.ManagedReference ||
+                !PropertyUtility.IsSerializableArrayElement(property))
+            {
+                return;
+            }
+
+            menu.AddItem(new GUIContent("Copy Reference"), false, () =>
+            {
+                //propertyToCopy = property;
+            });
+            //if (propertyToCopy != null)
+            //{
+            //    menu.AddItem(new GUIContent("Paste Reference"), false, () =>
+            //    {
+            //        property.serializedObject.Update();
+            //        //property.managedReferenceValue = null;
+            //        property.serializedObject.ApplyModifiedProperties();
+            //        propertyToCopy = null;
+            //    });
+            //}
+            //else
+            {
+                menu.AddDisabledItem(new GUIContent("Paste Reference"));
+            }
+
+            menu.AddItem(new GUIContent("Duplicate Reference"), false, () =>
+            {
+                var parent = property.GetParent();
+                parent.arraySize++;
+                parent.serializedObject.ApplyModifiedProperties();
+                var newProperty = parent.GetArrayElementAtIndex(parent.arraySize - 1);
+                //TODO: fill all serializable fields
+            });
+        }
+    }
+}
