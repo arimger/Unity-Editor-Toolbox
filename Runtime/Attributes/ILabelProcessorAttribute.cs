ea07043641c4a7750adc30ced71a0ed667713e4e
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2023-04-12 00:48:12 +0200

    Better support for the LabelByChildAttribute across Toolbox Editors

---
+++ Runtime/Attributes/ILabelProcessorAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Toolbox.Attributes
+{
+    /// <summary>
+    /// Temporary interface used to wrap all attributes used to override property labels.
+    /// </summary>
+    public interface ILabelProcessorAttribute
+    { }
+}
