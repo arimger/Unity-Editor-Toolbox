ed6167811880290a72e6429346f567b02c3b1fff
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2023-06-17 03:48:54 +0200

    Fix and optimize displaying Scene details

---
+++ Assets/Examples/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField, SceneDetails]
+    private SerializedScene scene;
+
+    private void Start()
+    {
+        SceneManager.LoadScene(scene.BuildIndex);
+    }
+}
