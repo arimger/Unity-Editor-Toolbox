ed500bb237ba03b0c8fa7f75639626cb1aeddd5f
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2023-12-10 15:10:14 +0100

    Implement 'Revert Prefab Name' option in the GameObject/Prefabs context menu

---
+++ Assets/Editor Toolbox/Editor/Utilities/PrefabUtility.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+
+namespace Toolbox.Editor.Utilities
+{
+    public static class PrefabUtility
+    {
+        [MenuItem("GameObject/Prefab/Revert Prefab Name", true, -100)]
+        public static bool ValidateRevertPrefabName()
+        {
+            var gameObjects = Selection.gameObjects;
+            for (int i = 0; i < gameObjects.Length; i++)
+            {
+                var gameObject = gameObjects[i];
+                if (UnityEditor.PrefabUtility.IsAnyPrefabInstanceRoot(gameObject))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        [MenuItem("GameObject/Prefab/Revert Prefab Name", false, -100)]
+        public static void RevertPrefabName()
+        {
+            var gameObjects = Selection.gameObjects;
+            Undo.RecordObjects(gameObjects, "Revert Prefab Name");
+            for (int i = 0; i < gameObjects.Length; i++)
+            {
+                var gameObject = gameObjects[i];
+                var prefabObject = UnityEditor.PrefabUtility.GetCorrespondingObjectFromSource(gameObject);
+                if (prefabObject == null)
+                {
+                    continue;
+                }
+
+                gameObject.name = prefabObject.name;
+            }
+        }
+    }
+}
