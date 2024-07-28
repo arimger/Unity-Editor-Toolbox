015d5dd6891c6ebec5d7173c255ef7022ae395d9
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2024-07-29 00:42:07 +0200

    Implement DisableInEditModeAttribute & Drawer

---
+++ Assets/Editor Toolbox/Editor/Drawers/Toolbox/Condition/DisableInEditModeAttributeDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Toolbox.Editor.Drawers
+{
+    public class DisableInEditModeAttributeDrawer : ToolboxConditionDrawer<DisableInEditModeAttribute>
+    {
+        protected override PropertyCondition OnGuiValidateSafe(SerializedProperty property, DisableInEditModeAttribute attribute)
+        {
+            return !EditorApplication.isPlayingOrWillChangePlaymode ? PropertyCondition.Disabled : PropertyCondition.Valid;
+        }
+    }
+}
---
+++ Assets/Editor Toolbox/Runtime/Attributes/Property/Toolbox/ConditionAttributes/DisableInEditModeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// Marks serialized field as read-only but only in the EditMode.
+    /// 
+    /// <para>Supported types: all.</para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    [Conditional("UNITY_EDITOR")]
+    public class DisableInEditModeAttribute : ToolboxConditionAttribute
+    { }
+}
