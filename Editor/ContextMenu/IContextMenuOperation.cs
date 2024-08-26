d0cf99585ad97340e7028338f6eeaf1ef1f56346
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2024-08-26 12:51:23 +0200

    Update project to Unity 2021.x; implement SerializeReference-based context menu operations (Copy, Paster, Duplicate)

---
+++ Editor/ContextMenu/IContextMenuOperation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Toolbox.Editor.ContextMenu
+{
+    internal interface IContextMenuOperation
+    {
+        bool IsVisible(SerializedProperty property);
+        bool IsEnabled(SerializedProperty property);
+        void Perform(SerializedProperty property);
+
+        GUIContent Label { get; }
+    }
+}
