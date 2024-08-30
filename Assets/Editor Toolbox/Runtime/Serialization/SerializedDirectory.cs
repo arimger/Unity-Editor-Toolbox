f5435d44c591c9eb3fa821cfb1e85a79e360952b
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2024-08-30 21:16:22 +0200

    Fix runtime compilation error caused by the SerializedDirectory

--- Assets/Editor Toolbox/Runtime/Serialization/SerializedDirectory.cs
+++ Assets/Editor Toolbox/Runtime/Serialization/SerializedDirectory.cs
@@ -16,6 +16,7 @@ namespace UnityEngine
         [SerializeField, Disable]
         private string path;
 
+#if UNITY_EDITOR
         internal static bool IsAssetValid(DefaultAsset asset, out string path)
         {
             path = asset != null ? AssetDatabase.GetAssetPath(asset) : null;
@@ -33,7 +34,7 @@ namespace UnityEngine
 
             return false;
         }
-
+#endif
         void ISerializationCallbackReceiver.OnAfterDeserialize()
         { }
 
