b43ce820defe5f041ac3a4f74f81d20e836ddbd5
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2025-07-03 23:06:19 +0200

    Add CustomSelectableEditor in the examples

---
+++ Assets/Examples/Editor/UI/CustomSelectableEditor.cs
@@ -0,0 +1,53 @@
+﻿using Toolbox.Editor;
+using Toolbox.Editor.Drawers;
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(CustomSelectable))]
+public class CustomSelectableEditor : SelectableEditor, IToolboxEditor
+{
+    private static readonly string[] selectableProperties = new string[]
+    {
+        "m_Script",
+        "m_Interactable",
+        "m_TargetGraphic",
+        "m_Transition",
+        "m_Colors",
+        "m_SpriteState",
+        "m_AnimationTriggers",
+        "m_Navigation"
+    };
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        foreach (var property in selectableProperties)
+        {
+            IgnoreProperty(property);
+        }
+    }
+
+    public sealed override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ToolboxEditorHandler.HandleToolboxEditor(this);
+    }
+
+    public void DrawCustomInspector()
+    {
+        Drawer.DrawEditor(serializedObject);
+    }
+
+    public void IgnoreProperty(SerializedProperty property)
+    {
+        Drawer.IgnoreProperty(property);
+    }
+
+    public void IgnoreProperty(string propertyPath)
+    {
+        Drawer.IgnoreProperty(propertyPath);
+    }
+
+    Editor IToolboxEditor.ContextEditor => this;
+    public IToolboxEditorDrawer Drawer { get; } = new ToolboxEditorDrawer();
+}
---
+++ Assets/Examples/Scripts/UI/CustomSelectable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomSelectable : Selectable
+{
+    private interface ICustomLogic
+    { }
+
+    [SerializeReference]
+    private ICustomLogic customLogic;
+}
